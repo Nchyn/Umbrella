cc841af90e4a75c0e2400d18fa7ed67603f617c4
Nchyn <6263075@163.com>
2018-08-10 09:37:30 +0800

    推送第三版，添加多用户支持

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Umbrella
+# Umbrella  
 一个PHP的小程序，可以设置定时发送邮件提醒带伞
+
+当前版本：Ver.3,已实现的功能有：多用户，可选雨天单独推送
 
 在 https://console.heweather.com/ 申请好KEY后
 
-于config.php内填写合适内容即可享用本脚本
+于config.php与user.json内填写合适内容即可享用本脚本
 
 # 简易使用说明：
 
@@ -17,9 +19,6 @@
 您需要按照实际需求填写
 
 ```
-$userid = "Nchyn";	//用户名
-$location = "shanghai";	//用户物理地址
-$usermail = "youremail@domain.com";	//用户邮箱
 $appname = "Nchyn's带伞提醒";	//应用名称
 $link = "http://weather.nchyn.com";	//应用主页
 $key = "your KEY";//和风天气KEY，请在api.hweather.com申请
@@ -40,9 +39,13 @@ $smtppass       = "yourpassword"; //SMTP服务器的用户密码
 `$smtpserver`添加`ssl://`,
 
 如：`$smtpserver     = "smtp.ym.163.com";`
-## 4.运行umbrellaV2.php
+
+## 4.编辑users.json
+### 注意检查
+
+## 5.运行umbrellaV3.php
 您可以部署于WEB服务器或
-`php /home/umb/umbrellaV2.php`
+`php /home/umb/umbrellaV3.php`
 直接运行
 亦可设置crontab做定时任务
 
--- config.php
+++ config.php
@@ -3,25 +3,21 @@
 date_default_timezone_set("PRC");
 
 require 'email.class.php';
-$userid = "Nchyn";	//用户名
-$location = "shanghai";	//用户物理地址
-$usermail = "youremail@domain.com";	//用户邮箱
 $appname = "Nchyn's带伞提醒";	//应用名称
 $link = "http://weather.nchyn.com";	//应用主页
-$key = "your KEY";//和风天气KEY，请在api.hweather.com申请
-$api = "https://free-api.heweather.com/s6/weather/forecast?location={$location}&key={$key}";//json地址
-$str = file_get_contents($api);
+$Hekey = "YOURKEY";//和风天气KEY，请在api.hweather.com申请
+
 //echo "邮件发送完成".date('时间：Y年m月d日   H:i');
 function sendmailto($mailto, $mailsub, $mailbd)
 {
     //require_once ('email.class.php');
     //##########################################
-    $smtpserver     = "smtp.ym.163.com"; //SMTP服务器,如果使用ssl,请加前缀ssl://
+    $smtpserver     = "smtp.ym.163.com"; //SMTP服务器，如使用ssl，请在下面一行参数前添加ssl://
     $smtpserverport = 25; //SMTP服务器端口
-    $smtpusermail   = "your@domain.com"; //SMTP服务器的用户邮箱
+    $smtpusermail   = "example@example.com"; //SMTP服务器的用户邮箱
     $smtpemailto    = $mailto;
-    $smtpuser       = "your@domain.com"; //SMTP服务器的用户帐号
-    $smtppass       = "yourpassword"; //SMTP服务器的用户密码
+    $smtpuser       = "example@example.com"; //SMTP服务器的用户帐号
+    $smtppass       = "example"; //SMTP服务器的用户密码
     $mailsubject    = $mailsub; //邮件主题
     $mailsubject    = "=?UTF-8?B?" . base64_encode($mailsubject) . "?="; //防止乱码
     $mailbody       = $mailbd; //邮件内容
@@ -29,7 +25,7 @@ function sendmailto($mailto, $mailsub, $mailbd)
     $mailtype       = "HTML"; //邮件格式（HTML/TXT）,TXT为文本邮件. 139邮箱的短信提醒要设置为HTML才正常
     ##########################################
     $smtp           = new smtp($smtpserver, $smtpserverport, true, $smtpuser, $smtppass); //这里面的一个true是表示使用身份验证,否则不使用身份验证.
-    $smtp->debug    = false; //是否显示发送的调试信息
+    $smtp->debug    = true; //是否显示发送的调试信息
     $smtp->sendmail($smtpemailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
      
 }
